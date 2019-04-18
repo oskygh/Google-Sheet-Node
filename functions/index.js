5e01302c57fd90800e07f342777772ae759e59cf
Osborne <osbornebm406@gmail.com>
2019-04-18 12:24:30 +0000

    Installed google-spreadsheet & promisfy

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const GoogleSpreadsheet = require("google-spreadsheet");
+const { promsify } = require("util");
+const cred = require("../functions/google-sheet.json");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
