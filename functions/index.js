00b8caed23c69b2f69fb9de969f1c5ea91db930a
Osborne <osbornebm406@gmail.com>
2019-04-18 13:24:33 +0000

    Implement Async Functions

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,26 @@ const cred = require("../functions/google-sheet.json");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.accessSpreadsheet = functions.https.onRequest(async (request, response) => {
+    try{
+        const getAccess = () => {
+            const doc = new GoogleSpreadsheet(
+              "1y6_zgdIDH4Qk5cRsF5zicyjpE40HbpjZBmUHn0elTPI"
+            );
+            await promsify(doc.useServiceAccountAuth)(cred)
+            const info = await promsify(doc.getInfo)()
+            const sheet = info.worksheet[0]
+            console.log(`Title : ${sheet.title} Rows : ${sheet.rowCount}`)
+          };
+    }
+    catch{
+      //@TODO error statement
+    }
+    finally {
+     console.log('Done')   
+    }
+   
+});
+
+
